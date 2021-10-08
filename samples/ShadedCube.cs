074b85814719a05994e33a6bd5d6f9bd6f71110c
Alexander Klingenbeck <alexander.klingenbeck@gmx.de>
2021-10-08 10:37:39 +0200

    Gltf loading prototype

--- samples/ShadedCube.cs
+++ samples/ShadedCube.cs
@@ -9,7 +9,7 @@ namespace net6test.samples
         
         float r = 0;
 
-        public void LoadModel(){
+        public Action LoadModel(){
             var model = ModelRoot.Load("assets/cube.glb");
             var prim = model.LogicalMeshes.First().Primitives.First();
             
@@ -23,6 +23,21 @@ namespace net6test.samples
             var normAccessor = prim.GetVertexAccessor("NORMAL");
             var normData = normAccessor.AsVector3Array().ToArray();
             var normBuf = GlUtil.CreateBuffer(GL.GL_ARRAY_BUFFER, normData);
+
+            return () => {
+                GL.glBindBuffer(GL.GL_ARRAY_BUFFER, posBuf);
+                shader.EnableAttribute(VertexAttributeType.Position, VertexAttributeDescriptor.Vec3f);
+
+                // GL.glBindBuffer(GL.GL_ARRAY_BUFFER, col);
+                // shader.EnableAttribute(VertexAttributeType.Color);
+
+                GL.glBindBuffer(GL.GL_ARRAY_BUFFER, normBuf);
+                shader.EnableAttribute(VertexAttributeType.Normal);
+
+                GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, idxBuf);
+
+                GL.glDrawElements(GL.GL_TRIANGLES, (uint)indices.Length, GL.GL_UNSIGNED_SHORT, IntPtr.Zero);
+            };
         }
 
         // See: https://github.com/bonigarcia/webgl-examples/blob/master/lighting/relistic_shading.html
@@ -30,23 +45,13 @@ namespace net6test.samples
         {
             shader = new Shader(vshader, fshader, new() { 
                 [VertexAttributeType.Position] = "a_Position",
-                [VertexAttributeType.Normal] = "a_Normal",
-                [VertexAttributeType.Color] = "a_Color"
+                [VertexAttributeType.Normal] = "a_Normal"
             });
             shader.Use();
             GL.glEnable(GL.GL_DEPTH_TEST);
-            GL.glEnable(GL.GL_CULL_FACE);
-            
-            var model = ModelRoot.Load("assets/cube.glb");
-            model.LogicalMeshes.First().Primitives.First();
-                        
-            pos = GlUtil.CreateBuffer(GL.GL_ARRAY_BUFFER, vertices);
-            norm = GlUtil.CreateBuffer(GL.GL_ARRAY_BUFFER, normals);
-            col = GlUtil.CreateBuffer(GL.GL_ARRAY_BUFFER, colors);
-             
-            idx = GlUtil.CreateBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, indices);
-
-            LoadModel();
+            GL.glEnable(GL.GL_CULL_FACE);                                    
+          
+            drawAction = LoadModel();
         }
 
         public void Update()
@@ -72,18 +77,7 @@ namespace net6test.samples
             var lightColor = new Vector3(1,1,1);
             shader.SetUniform("u_LightColor", lightColor);
 
-            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, pos);
-            shader.EnableAttribute(VertexAttributeType.Position, VertexAttributeDescriptor.Vec3f);
-
-            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, col);
-            shader.EnableAttribute(VertexAttributeType.Color);
-
-            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, norm);
-            shader.EnableAttribute(VertexAttributeType.Normal);
-
-            GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, idx);
-
-            GL.glDrawElements(GL.GL_TRIANGLES, 6*2*3, GL.GL_UNSIGNED_BYTE, IntPtr.Zero);
+            drawAction();
         }
 
         string vshader = @"attribute vec3 a_Color;
@@ -94,68 +88,30 @@ namespace net6test.samples
     uniform mat4 u_vMatrix;
     uniform mat4 u_mvMatrix;
 
-    varying highp vec4 v_Color;
+    //varying highp vec4 v_Color;
     varying highp vec3 v_Normal;
 
     void main(void) {
         gl_Position = u_pMatrix * u_vMatrix * u_mvMatrix * a_Position;
         v_Normal = normalize(a_Normal - vec3(gl_Position));
-        v_Color = vec4(a_Color, 1.0);
+        //v_Color = vec4(a_Color, 1.0);
     }";
         string fshader = @"precision mediump float;
 
     uniform vec3 u_LightColor;
     uniform vec3 u_LightDirection;
 
-    varying highp vec4 v_Color;
+    //varying highp vec4 v_Color;
     varying highp vec3 v_Normal;
 
     void main(void) {
         float nDotL = max(dot(u_LightDirection, v_Normal), 0.0);
 
-        vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;
-        gl_FragColor = vec4(diffuse, v_Color.a);
+        vec3 diffuse = u_LightColor/* * v_Color.rgb*/ * nDotL;
+        gl_FragColor = vec4(diffuse, 1.0);
     }";
-
-        float[] vertices = new float[]{ // Coordinates
-      0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, // v0-v1-v2-v3 front
-      0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, // v0-v3-v4-v5 right
-      0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, // v0-v5-v6-v1 up
-      -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, // v1-v6-v7-v2 left
-      -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, // v7-v4-v3-v2 down
-      0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f // v4-v7-v6-v5 back
-    };
-
-        float[] colors = new float[]{ // Colors
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // v0-v1-v2-v3 front
-      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, // v0-v3-v4-v5 right
-      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, // v0-v5-v6-v1 up
-      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, // v1-v6-v7-v2 left
-      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, // v7-v4-v3-v2 down
-      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 // v4-v7-v6-v5 back
-      };
-
-        float[] normals = new float[]{ // Normal
-      0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // v0-v1-v2-v3 front
-      1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // v0-v3-v4-v5 right
-      0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // v0-v5-v6-v1 up
-      -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // v1-v6-v7-v2 left
-      0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, // v7-v4-v3-v2 down
-      0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f // v4-v7-v6-v5 back
-      };
-
-        byte[] indices = new byte[]{ 0, 1, 2, 0, 2, 3, // front
-      4, 5, 6, 4, 6, 7, // right
-      8, 9, 10, 8, 10, 11, // up
-      12, 13, 14, 12, 14, 15, // left
-      16, 17, 18, 16, 18, 19, // down
-      20, 21, 22, 20, 22, 23 // back
-      };
         private Shader shader;
-        private uint pos;
-        private uint norm;
-        private uint col;
-        private uint idx;
+        private Action drawAction;
         private Matrix4x4 matP;
         private Matrix4x4 matV;
         private Matrix4x4 matM;
